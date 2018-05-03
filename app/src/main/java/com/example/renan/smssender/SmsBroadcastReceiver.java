f615400bf777da53b455fd265fc349bd69681dc8
Renan Gimenez <renan.gimenez@hotmail.fr>
2018-05-03 15:30:46 +0200

    receive SMS ok

---
+++ app/src/main/java/com/example/renan/smssender/SmsBroadcastReceiver.java
@@ -0,0 +1,38 @@
+package com.example.renan.smssender;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+    public static final String SMS_BUNDLE = "pdus";
+
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+
+            String address = "", smsBody = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                smsBody = smsMessage.getMessageBody().toString();
+                address = smsMessage.getOriginatingAddress();
+
+                smsMessageStr += "SMS From: " + address + "\n";
+                smsMessageStr += smsBody + "\n";
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+            Log.d("message",smsMessageStr);
+            SocketServerThread.sendToComputer(address, smsBody);
+            //this will update the UI with message
+           /* SmsActivity inst = SmsActivity.instance();
+            inst.updateList(smsMessageStr);*/
+        }
+    }
+}
